3af2211bb9c7eb979abbd42fb8f47506f96aef72
Taby <forwardpivot@proton.me>
2022-10-20 12:34:56 -0500

    Added lines

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ ctx.fillRect(100, 100, 100, 100);
 ctx.fillRect(200, 200, 100, 100);
 ctx.fillRect(300, 300, 100, 100);
 ctx.fillRect(400, 400, 100, 100);
+
+ctx.beginPath();
+ctx.moveTo(110,300);
+ctx.lineTo(210,410);
+ctx.lineTo(210, 650);
+ctx.lineTo(510, 50);
+ctx.strokeStyle = "#da6731"
+ctx.stroke();
