208d4402b0e565f6cfbfc9ab3f35a8fcd9b90fd6
Taby <forwardpivot@proton.me>
2022-10-20 12:00:34 -0500

    added basic html, linked all the files, and added the canvas element. Also made the canvas element fit the screen

---
+++ script.js
@@ -0,0 +1,4 @@
+const resizeCanvas = document.querySelector('#resizeCanvas');
+//Resizes the canvas to fit the screen
+resizeCanvas.width = window.innerWidth;
+resizeCanvas.height = window.innerHeight;
