7e81f4e8090350a79bd35481874cf01943eb11ba
Taby <forwardpivot@proton.me>
2022-10-20 15:34:35 -0500

    Added random circles

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ ctx.lineTo(210,410);
 ctx.lineTo(210, 650);
 ctx.lineTo(510, 50);
 ctx.strokeStyle = "#da6731"
+ctx.closePath(); //This connects the first, and last points of a line
 ctx.stroke();
 
 //arc
@@ -34,6 +35,17 @@ for (let i = 0, x = 200, y = 200 ; i < 5; i++) {
     ctx.beginPath();
     ctx.arc(x, y, 30, 0, Math.PI * 2);
     ctx.strokeStyle = 'red';
+    ctx.stroke();
+    x += 100;
+    y += 100;
+}
+
+for (let i = 0; i < 5; i++) {
+    let x = Math.random() * innerWidth;
+    let y = Math.random() * innerHeight;
+    ctx.beginPath();
+    ctx.arc(x, y, 30, 0, Math.PI * 2);
+    ctx.strokeStyle = 'orange';
     ctx.stroke();
     x += 100;
     y += 100;
